26dd3342f1004b5f4da311264d36aad951b681d9
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-04-18 01:45:52 +0900

    Navbarにそこまで力を入れても仕方がないのでここでいったん止める

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,7 @@ import style from "@/styles/navbar.module.css";
 export default function Navbar() {
     return (
         <nav className={style.container}>
-            <h1 style={{color: "black"}}>Navbar</h1>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-            </ul>
+            <h1>Japan Prefecture Population Chart</h1>
         </nav>
     );
 }
