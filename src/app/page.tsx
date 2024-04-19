76f216e422c047554aa26a777be2219c41e7a2c9
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-04-19 19:46:51 +0900

    表示確認のためにコンポーネントを作成、Footerと表示が重なってしまうので、Footer側の修正を要求したいと考える

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Chart from "@/components/Chart"
-import Form from "@/components/Form"
-import RESAS from "@/resas"
-import { Prefecture, PrefectureResponse } from "@/types/resas"
+// import Form from "@/components/Form"
+import TestForm from "@/components/TestForm"
+// import RESAS from "@/resas"
+// import {  Prefecture, PrefectureResponse } from "@/types/resas"
 
 export default async function Home() {
-  const Resas = RESAS()
-  const Prefectures = (await Resas({ name: "prefectures" })) as unknown as PrefectureResponse
+  /* const Resas = RESAS()
+  const Prefectures = (await Resas({ name: "prefectures" })) as unknown as PrefectureResponse */
   return (
     <main
       style={{
@@ -13,7 +14,8 @@ export default async function Home() {
       }}
     >
       <Chart />
-      <Form Prefectures={Prefectures.result as Prefecture[]} />
+      {/* <Form Prefectures={Prefectures.result as Prefecture[]} /> */}
+      <TestForm />
     </main>
   )
 }
