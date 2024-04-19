ce63c3d7b202628d66ebea4991ed454dedbdbf70
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-04-19 19:53:28 +0900

    コンフリクト解消時に誤ったコードを貼り付けてしまいました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,21 @@
-"use client"
-import { useEffect, useRef } from "react"
-import {
-  Path,
-  useForm,
-  // SubmitHandler,
-  UseFormRegister,
-  FieldValues
-} from "react-hook-form"
-import style from "@/styles/form.module.css"
-import { Prefecture } from "@/types/resas"
+import Chart from "@/components/Chart"
+// import Form from "@/components/Form"
+import TestForm from "@/components/TestForm"
+// import RESAS from "@/resas"
+// import {  Prefecture, PrefectureResponse } from "@/types/resas"
 
-export default function Form({ Prefectures }: { Prefectures: Prefecture[] }) {
-  const PrefectureNames = Prefectures.map(pref => pref.prefName)
-  const ConstPrefectureNames = [...PrefectureNames] as const
-
-  interface PrefectureSelectState {
-    SelectPrefectures: {
-      [key in (typeof ConstPrefectureNames)[number]]: boolean
-    }
-  }
-
-  interface CheckBoxProps<T extends FieldValues> {
-    register: UseFormRegister<T>
-    path: Path<T>
-    label: string
-  }
-
-  const DefaultValues: PrefectureSelectState = {
-    SelectPrefectures: {
-      ...PrefectureNames.reduce(
-        (properties, prefName) => {
-          properties[prefName] = false
-          return properties
-        },
-        {} as PrefectureSelectState["SelectPrefectures"]
-      )
-    }
-  }
-
-  const { register, handleSubmit, watch } = useForm<PrefectureSelectState>({
-    defaultValues: DefaultValues
-  })
-
-  function CheckBox<T extends FieldValues>({ register, path, label }: CheckBoxProps<T>) {
-    const LabelRef = useRef<HTMLLabelElement>(null)
-    useEffect(() => {
-      const LabelBackgroundColor = watch(`SelectPrefectures.${label}`) ? "red" : "darkgray"
-      if (LabelRef.current === null) {
-        throw new Error("label要素のRefが正常に処理されていません")
-      }
-      LabelRef.current.style.backgroundColor = LabelBackgroundColor
-    }, [watch(`SelectPrefectures.${label}`)])
-
-    return (
-      <label ref={LabelRef} className={style["checkbox"]}>
-        <input hidden type="checkbox" {...register(path)} />
-        {label}
-      </label>
-    )
-  }
-
+export default async function Home() {
+  /* const Resas = RESAS()
+  const Prefectures = (await Resas({ name: "prefectures" })) as unknown as PrefectureResponse */
   return (
-    <form
-      className={style.container}
-      onSubmit={handleSubmit(async data => {
-        console.log(data)
-      })}
+    <main
+      style={{
+        height: "60vh"
+      }}
     >
-      <div className={style["checkbox-container"]}>
-        {PrefectureNames.map(prefName => {
-          return <CheckBox key={prefName} register={register} path={`SelectPrefectures.${prefName}`} label={prefName} />
-        })}
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+      <Chart />
+      {/* <Form Prefectures={Prefectures.result as Prefecture[]} /> */}
+      <TestForm />
+    </main>
   )
 }
