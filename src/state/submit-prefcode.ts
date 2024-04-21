06057360b5f3583230ebd4922456d784bf162619
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-04-21 22:34:26 +0900

    Stateの初期値の設定が誤っていたので修正

--- src/state/submit-prefcode.ts
+++ src/state/submit-prefcode.ts
@@ -6,4 +6,10 @@ export type PrefState = {
     pref: Prefecture[]
 }
 
-export const initialPrefState = {pref: []} as PrefState
+export const initialPrefState = {pref: [{
+    prefCode: 27,
+    prefName: "大阪県"
+},{
+    prefCode: 13,
+    prefName: "東京都"
+}]} as PrefState
