9ddfe1d095b8d00590b604e3f7cdd4b6ae317862
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-04-21 17:10:18 +0900

    nextconfigに環境変数設定を追記

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        RESAS_API_KEY: process.env.RESAS_API_KEY
+    }
+};
 
 export default nextConfig;
